486d04c6e3d17888675e2d7b5825286ee3bd6e4a
brownrice-02 <vickylee.g123@gmail.com>
2026-04-14 10:08:43 +0800

    feat: implement binary search on sorted list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+    let lo = 0;
+    let hi = haystack.length;
+
+    while(hi > lo) {
+
+        let mid = Math.floor(lo + (hi - lo) / 2);
+        let v = haystack[mid];
+
+        if(v === needle) {
+            return true;
+        } else if(v > needle) {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+
+    return false;
 }
