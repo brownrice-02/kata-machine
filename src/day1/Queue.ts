3633bfa05a0e1ba386d7be755ac0b9ec6bf1ac07
brownrice-02 <vickylee.g123@gmail.com>
2026-05-25 10:17:36 +0800

    feat: implement Queue with fix for tail pointer not clearing on empty
    Reset tail to undefined when dequeue empties the queue, so subsequent
    enqueue calls correctly reinitialize both head and tail pointers.

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,53 @@
+type QueueNode<T> = {
+    value: T,
+    next?: QueueNode<T>,
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: QueueNode<T>;
+    private tail?: QueueNode<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = {value: item} as QueueNode<T>;
 
-}
+        this.length++;
+
+        if(!this.tail) {
+            this.tail = this.head = node
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+
     deque(): T | undefined {
+        if(!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+
+        const h = this.head;
+        this.head = this.head.next;
+
+        if (!this.head) {
+            this.tail = undefined;
+        }
+
+        // free
+        h.next = undefined;
+
+        return h.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
